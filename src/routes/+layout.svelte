2319cf780ab41f7775ee6818a212ace470e2b8d0
marianialessandro <alessandro@marianialessandro.com>
2025-08-09 15:17:51 +0200

    footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
 
 	<footer>
 		<p>
-			visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
+			Copyright © 2025 Alessandro Mariani All rights reserved
 		</p>
 	</footer>
 </div>
