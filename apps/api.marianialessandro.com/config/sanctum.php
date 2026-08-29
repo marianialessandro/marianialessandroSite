c6bc5ddf92fdd9dc34e4a6e77a6f59c851125845
Alessandro Mariani <59537682+marianialessandro@users.noreply.github.com>
2026-08-29 19:05:34 +0200

    fix(auth): remove nonexistent app subdomain

--- apps/api.marianialessandro.com/config/sanctum.php
+++ apps/api.marianialessandro.com/config/sanctum.php
@@ -20,7 +20,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s,%s',
-        'localhost,localhost:5175,localhost:5176,127.0.0.1:5175,127.0.0.1:5176,blog.marianialessandro.com,apps.marianialessandro.com,app.marianialessandro.com',
+        'localhost,localhost:5175,localhost:5176,127.0.0.1:5175,127.0.0.1:5176,blog.marianialessandro.com,apps.marianialessandro.com',
         Sanctum::currentApplicationUrlWithPort(),
     ))),
 
